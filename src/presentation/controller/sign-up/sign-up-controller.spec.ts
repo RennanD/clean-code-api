31eac6a4ad5d76bd8d8999e1d58f05f01ce5bcb6
rennandouglas <rennan.douglas@objetivopi.com.br>
2022-04-03 20:59:03 -0300

    chore: should return 400 if no name is provided

---
+++ src/presentation/controller/sign-up/sign-up-controller.spec.ts
@@ -0,0 +1,18 @@
+import { SignUpController } from './sign-up-controller';
+
+describe('SignUp Controller', () => {
+  it('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController();
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_name',
+        passwordConfimation: 'any_name',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
