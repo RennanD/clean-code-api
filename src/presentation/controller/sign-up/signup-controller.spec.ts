f49e8d2f78e75ad057a61186c8c461ced1d09dcc
rennandouglas <rennan.douglas@objetivopi.com.br>
2022-04-03 22:35:56 -0300

    feat: should return 500 if email validator throws

--- src/presentation/controller/sign-up/signup-controller.spec.ts
+++ src/presentation/controller/sign-up/signup-controller.spec.ts
@@ -1,5 +1,6 @@
 import { InvalidParamError } from '../../errors/invalid-param-error';
 import { MissingParamError } from '../../errors/missing-param-error';
+import { ServerError } from '../../errors/server-error';
 import { IEmailValidator } from '../../protocols/email-validator';
 import { SignUpController } from './signup-controller';
 
@@ -125,5 +126,29 @@ describe('SignUp Controller', () => {
     sut.handle(httpRequest);
 
     expect(isValidSpy).toHaveBeenCalledWith('any_email@email.com');
+  });
+
+  it('should return 500 if email validator throws', () => {
+    class EmailValidatorStub implements IEmailValidator {
+      isValid(email: string): boolean {
+        throw new Error();
+      }
+    }
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+
+    const emailValidatorStub = new EmailValidatorStub();
+    const sut = new SignUpController(emailValidatorStub);
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
   });
 });
